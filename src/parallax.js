fe9f1d543543100b8c99ab37105ed9e0f64d6e71
Jenna Ransom <jenna.ransom@gmail.com>
2021-08-13 00:14:37 -0600

    changes background color of section on scroll

--- src/parallax.js
+++ src/parallax.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sections = document.querySelectorAll('section')
+  const bodyTag = document.querySelector('body')
 
-  const addMovement = function() {
+  const addMovement = function () {
     const topViewpoint = window.pageYOffset
-    const midViewpoint = topViewpoint + window.innerHeight / 2
+    const midViewpoint = topViewpoint + (window.innerHeight / 2)
     
     sections.forEach((s, i) => {
       const topSect = s.offsetTop
@@ -14,13 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
       const content = s.querySelector('div')
 
       let rotation = distanceToSection / 100
-      let contentDist = -1 * (distanceToSection / 2)
+      let contentDist = -1 * distanceToSection / 2
 
-      if (i % 2 === 0) { rotation = rotation * -1 }
+      if (i % 2 == 1) { rotation = rotation * -1 }
 
       image.style.transform = `rotate(${rotation}deg)`
       content.style.top = `${contentDist}px`
       content.style.transform = `rotate(${-1 * rotation}deg)`
+
+      if (distanceToSection > -100) { 
+        bodyTag.style.backgroundColor = s.getAttribute('data-background')
+      }
     })
 
   }
